b55dc4403bed1be4527166ed69e23d4ac289aa61
LCima <49267461+LCima@users.noreply.github.com>
2020-06-05 16:39:23 +0200

    Eliminati gli attributi non utilizzati da restaurant (client)

--- Ristogo3/src/ristogo/ui/graphics/beans/RestaurantBean.java
+++ Ristogo3/src/ristogo/ui/graphics/beans/RestaurantBean.java
@@ -4,6 +4,7 @@ import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import ristogo.common.entities.Restaurant;
+import ristogo.common.entities.enums.Cuisine;
 import ristogo.common.entities.enums.Genre;
 import ristogo.common.entities.enums.OpeningHours;
 import ristogo.common.entities.enums.Price;
@@ -12,44 +13,34 @@ public class RestaurantBean extends EntityBean
 {
 	private final SimpleStringProperty name;
 	private final SimpleStringProperty ownerName;
-	private final SimpleObjectProperty<Genre> genre;
+	private final SimpleObjectProperty<Cuisine> cuisine;
 	private final SimpleObjectProperty<Price> price;
 	private final SimpleStringProperty city;
-	private final SimpleStringProperty address;
 	private final SimpleStringProperty description;
-	private final SimpleIntegerProperty seats;
-	private final SimpleObjectProperty<OpeningHours> openingHours;
 
-	public RestaurantBean(int id, String name, String ownerName, Genre genre,
-		Price price, String city, String address, String description,
-		int seats, OpeningHours openingHours)
+	public RestaurantBean(int id, String name, String ownerName, Cuisine cuisine,
+		Price price, String city, String description)
 	{
 		super(id);
 		this.name = new SimpleStringProperty(name);
 		this.ownerName = new SimpleStringProperty(ownerName);
-		this.genre = new SimpleObjectProperty<Genre>(genre);
+		this.cuisine = new SimpleObjectProperty<Cuisine>(cuisine);
 		this.price = new SimpleObjectProperty<Price>(price);
 		this.city = new SimpleStringProperty(city);
-		this.address = new SimpleStringProperty(address);
 		this.description = new SimpleStringProperty(description);
-		this.seats = new SimpleIntegerProperty(seats);
-		this.openingHours = new SimpleObjectProperty<OpeningHours>(openingHours);
 	}
-
+	
 	public static RestaurantBean fromEntity(Restaurant restaurant)
 	{
 		return new RestaurantBean(restaurant.getId(), restaurant.getName(),
-			restaurant.getOwnerName(), restaurant.getGenre(), restaurant.getPrice(),
-			restaurant.getCity(), restaurant.getAddress(),
-			restaurant.getDescription(), restaurant.getSeats(),
-			restaurant.getOpeningHours());
+			restaurant.getOwnerName(), restaurant.getCuisine(), restaurant.getPrice(),
+			restaurant.getCity(), restaurant.getDescription());
 	}
 
 	public Restaurant toEntity()
 	{
-		return new Restaurant(getId(), getName(), getOwnerName(), getGenre(),
-			getPrice(), getCity(), getAddress(), getDescription(),
-			getSeats(), getOpeningHours());
+		return new Restaurant(getId(), getName(), getOwnerName(), getCuisine(),
+			getPrice(), getCity(), getDescription());
 	}
 
 	public String getOwnerName()
@@ -62,24 +53,14 @@ public class RestaurantBean extends EntityBean
 		return name.get();
 	}
 
-	public Genre getGenre()
+	public Cuisine getCuisine()
 	{
-		return genre.get();
-	}
-
-	public int getSeats()
-	{
-		return seats.get();
+		return cuisine.get();
 	}
 
 	public String getCity()
 	{
 		return city.get();
-	}
-
-	public String getAddress()
-	{
-		return address.get();
 	}
 
 	public String getDescription()
@@ -92,11 +73,6 @@ public class RestaurantBean extends EntityBean
 		return price.get();
 	}
 
-	public OpeningHours getOpeningHours()
-	{
-		return openingHours.get();
-	}
-
 	public void setOwnerName(String ownerName)
 	{
 		this.ownerName.set(ownerName);
@@ -107,24 +83,14 @@ public class RestaurantBean extends EntityBean
 		this.name.set(name);
 	}
 
-	public void setGenre(Genre genre)
+	public void setCuisine(Cuisine cuisine)
 	{
-		this.genre.set(genre);
-	}
-
-	public void setSeats(int seats)
-	{
-		this.seats.set(seats);
+		this.cuisine.set(cuisine);
 	}
 
 	public void setCity(String city)
 	{
 		this.city.set(city);
-	}
-
-	public void setAddress(String address)
-	{
-		this.address.set(address);
 	}
 
 	public void setDescription(String description)
@@ -137,8 +103,4 @@ public class RestaurantBean extends EntityBean
 		this.price.set(price);
 	}
 
-	public void setOpeningHours(OpeningHours openingHours)
-	{
-		this.openingHours.set(openingHours);
-	}
 }
