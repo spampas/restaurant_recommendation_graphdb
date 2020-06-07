f5209658df667b0ae2ab74ec83d86a1b57adc59f
LCima <49267461+LCima@users.noreply.github.com>
2020-06-07 11:53:42 +0200

    Create ButtonForm.java

---
+++ Ristogo3/src/ristogo/ui/graphics/ButtonForm.java
@@ -0,0 +1,82 @@
+package ristogo.ui.graphics;
+
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import ristogo.ui.graphics.config.GUIConfig;
+
+public class ButtonForm extends VBox{
+
+	private String type;
+	private Button myButton;
+	private Button findButton;
+	private Button recommendedButton;
+	
+	public ButtonForm(String type)
+	{
+		this.type = type;
+		
+		myButton = new Button();
+		myButton.setFont(GUIConfig.getButtonFont());
+		myButton.setTextFill(GUIConfig.getInvertedFgColor());
+		myButton.setStyle(GUIConfig.getInvertedCSSButtonBgColor());
+		
+		findButton = new Button();
+		findButton.setFont(GUIConfig.getButtonFont());
+		findButton.setTextFill(GUIConfig.getInvertedFgColor());
+		findButton.setStyle(GUIConfig.getInvertedCSSButtonBgColor());
+		
+		recommendedButton = new Button();
+		recommendedButton.setFont(GUIConfig.getButtonFont());
+		recommendedButton.setTextFill(GUIConfig.getInvertedFgColor());
+		recommendedButton.setStyle(GUIConfig.getInvertedCSSButtonBgColor());
+		
+		switch(type)
+		{
+			case "friend": 
+				initializeButtonFriend();
+				break;
+			case "restaurant":
+				initializeButtonRestaurant();
+				break;
+		}
+		
+		
+	}
+
+	private void initializeButtonRestaurant() {
+		
+		myButton.setText("View my friends");
+		findButton.setText("Find friends");
+		recommendedButton.setText("View recommended friends");
+	}
+
+	private void initializeButtonFriend() {
+		
+		myButton.setText("View my favourite restaurants");
+		findButton.setText("Find restaurants");
+		recommendedButton.setText("View recommended restaurants");
+		
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Button getMyButton() {
+		return myButton;
+	}
+
+	public Button getFindButton() {
+		return findButton;
+	}
+
+	public Button getRecommendedButton() {
+		return recommendedButton;
+	}
+
+	
+}
